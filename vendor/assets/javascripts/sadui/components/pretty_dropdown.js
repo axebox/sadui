4df9699de2ec976c25a759859735107f9796d595
Ethan Tabor <etabor@axebox.com>
2014-03-19 09:46:45 -0700

    Pretty dropdown

--- vendor/assets/javascripts/sadui/components/pretty_dropdown.js
+++ vendor/assets/javascripts/sadui/components/pretty_dropdown.js
@@ -1,15 +1,37 @@
 /* 
  * Pretty Dropdown
  * 
- *
  * Requires _pretty_dropdown.scss
+ *
+ * Example DOM:
+ * div.pretty-dropdown
+ *   select.is-dropdown
+ *     option
+ *     option.is-href
+ *   div.pretty-dropdown-layer
+ *     div.pretty-dropdown-label.is-label
+ *     span.pretty-dropdown-icon-container
+ *       span.pretty-dropdown-icon &gt;
+ * 
+ * API accessible through $el.data('prettyDropdown')
+ *
+ * opts Object
+ * opts.$el $ DOM Object
+ * opts.autoinit Boolean [true]
  */
-sadui.pretty_dropdown = function(conf){
 
-    var $label = $('.dropdown-label', conf.$dropdown);
+sadui.pretty_dropdown = function(opts){
+
+    var defaults = {
+        autoinit: true
+    };
+
+    var conf = $.extend(defaults, opts);
+
+    var $label = $('.is-label', conf.$el);
 
     var bind = function(){
-        $('.is-dropdown', conf.$dropdown).on('change', function(ev){
+        $('.is-dropdown', conf.$el).on('change', function(ev){
 
             var $this = $(this);
             var $item = $('option:selected', $this);
@@ -19,6 +41,7 @@ sadui.pretty_dropdown = function(conf){
             // update label
             $label.text( value );
 
+            // trigger location if has href
             if ($item.hasClass('is-href')) {
                 document.location = $item.val();
             }
@@ -29,15 +52,25 @@ sadui.pretty_dropdown = function(conf){
         });
     };
 
+    var update_data = function(){
+        conf.$el.data('prettyDropdown', conf);
+    };
+
     var init = function(){
         bind();
 
-        var value = $('.is-dropdown option:selected', conf.$dropdown).text();
+        var value = $('.is-dropdown option:selected', conf.$el).text();
         
         // update label
         $label.text( value );
     };
 
-    init();
+    if (conf.autoinit) {
+        init();
+    }
+
+    update_data();
+
+    return conf;
 
 };
