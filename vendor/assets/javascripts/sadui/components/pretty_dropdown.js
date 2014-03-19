7f964b0e7ff72392bff2e87b472c705ee9bd72e7
Ethan Tabor <etabor@axebox.com>
2014-03-19 15:11:49 -0700

    Pretty dropdown with menu

--- vendor/assets/javascripts/sadui/components/pretty_dropdown.js
+++ vendor/assets/javascripts/sadui/components/pretty_dropdown.js
@@ -13,6 +13,8 @@
  *     span.pretty-dropdown-icon-container
  *       span.pretty-dropdown-icon &gt;
  * 
+ * You may like to prepopulate .is-label element on server output
+ * 
  * API accessible through $el.data('prettyDropdown')
  *
  * opts Object
@@ -28,10 +30,20 @@ sadui.pretty_dropdown = function(opts){
 
     var conf = $.extend(defaults, opts);
 
-    var $label = $('.is-label', conf.$el);
+    conf.$label = $('.is-label', conf.$el);
+    conf.$dropdown = $('.is-dropdown', conf.$el);
+
+    conf.$menu = $('.is-menu', conf.$el);
+    conf.hasMenu = (conf.$menu && typeof conf.$menu === 'object' && conf.$menu.length > 0) ? true : false;
 
     var bind = function(){
-        $('.is-dropdown', conf.$el).on('change', function(ev){
+
+        // Bind <select> dropdown 
+        conf.$dropdown.on('change.sadui.prettyDropdown', function(ev){
+
+            if (conf.hasMenu) {
+                ev.preventDefault();
+            }
 
             var $this = $(this);
             var $item = $('option:selected', $this);
@@ -39,7 +51,8 @@ sadui.pretty_dropdown = function(opts){
             var value = $item.text();
 
             // update label
-            $label.text( value );
+            update_label();
+            // conf.$label.text( value );
 
             // trigger location if has href
             if ($item.hasClass('is-href')) {
@@ -50,6 +63,74 @@ sadui.pretty_dropdown = function(opts){
             $this.parent()[ ($this.is('[disabled]')) ? 'addClass' : 'removeClass']('is-disabled');
 
         });
+
+        // Bind menu
+        if (conf.hasMenu) {
+            conf.$el.on('click.sadui.prettyDropdown', '.is-label, .is-menu-item', function(ev){
+                ev.preventDefault();
+
+                if ($(ev.target).hasClass('is-label')) {
+
+                    if (!conf.$menu.hasClass('is-visible')) {
+                        conf.$menu.addClass('is-visible');
+
+                    } else {
+                        conf.$menu.removeClass('is-visible');
+                    }
+
+                } else if ($(ev.target).hasClass('is-menu-item')) {
+
+                    update_dropdown(ev);
+
+                    $('.is-menu-item', conf.$menu).removeClass('is-selected');
+                    $(this).addClass('is-selected');
+
+                    conf.$menu.removeClass('is-visible');
+                }
+
+            });
+
+
+        }
+    };
+
+    // @Param ev Object the event object of selected menu item
+    var update_dropdown = function(ev){
+        var el = $(ev.target);
+        
+        // Select form element
+        conf.$dropdown.val( el.data('value') );
+
+        conf.$dropdown.trigger('change.sadui.prettyDropdown');
+    };
+
+    var update_label = function(){
+        var label_value = $('.is-dropdown option:selected', conf.$el).text();
+        conf.$label.html( label_value );
+    };
+
+    var populate_menu = function(){
+        conf.values = $.map( $('option', conf.$dropdown), function(option){
+            var $option = $(option);
+            return {
+                value: $option.val(),
+                name: $option.text(),
+                selected: (typeof $option.attr('selected') !== 'undefined') ? true:false
+            }
+        });
+
+        // Clear current items
+        conf.$menu.html('');
+        
+        $.each(conf.values, function(i,attr){
+            var li = $('<li>')
+                .addClass('is-menu-item')
+                .attr('data-value', attr.value )
+                .html( attr.name )
+                .appendTo(conf.$menu);
+
+            if (attr.selected) li.addClass('is-selected');
+        });
     };
 
     var update_data = function(){
@@ -59,10 +140,15 @@ sadui.pretty_dropdown = function(opts){
     var init = function(){
         bind();
 
-        var value = $('.is-dropdown option:selected', conf.$el).text();
-        
-        // update label
-        $label.text( value );
+        if (conf.hasMenu) {
+            conf.$el.addClass('has-menu');
+
+            // Populate Menu
+            populate_menu();
+        }
+
+        update_label();
+
     };
 
     if (conf.autoinit) {
