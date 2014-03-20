519a4d65311025245c69e23bf3b7eca10bf4b010
Ethan Tabor <etabor@axebox.com>
2014-03-19 17:32:01 -0700

    Attempt to fix compass config

--- lib/sadui.rb
+++ lib/sadui.rb
@@ -1,5 +1,5 @@
 require "sadui/version"
-require 'compass'
+require 'compass-rails'
 
 # Used to ensure that Rails 3.0.x, as well as Rails >= 3.1 with asset pipeline disabled
 # get the minified version of the scripts included into the layout in production.
